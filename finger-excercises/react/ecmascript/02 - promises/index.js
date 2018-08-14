573ac5a5352910137a083b5b718ddfc8c84f4822
Giuliano Scaglioni <giuliano.scaglioni@wolox.com.ar>
2018-08-14 14:27:30 -0300

    Pass test 9
    - delay works well

--- finger-excercises/react/ecmascript/02 - promises/index.js
+++ finger-excercises/react/ecmascript/02 - promises/index.js
@@ -1,7 +1,15 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const delayPromise = new Promise((resolve) => {
+    const startTime = Date.now();
+    let endTime = Date.now();
+    while (startTime + time > endTime) {
+      endTime = Date.now();
+    }
+    return resolve(endTime - startTime);
+  });
+  return delayPromise;
 }
 
 export function asyncDelay() {
