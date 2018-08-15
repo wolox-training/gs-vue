1d99729c2fb6e8de0f47dff5715a842a4bdf3942
Giuliano Scaglioni <giuliano.scaglioni@wolox.com.ar>
2018-08-15 14:55:10 -0300

    Lift state up from Board to Game
    - Add history array

--- training-react/src/app/components/Board/index.js
+++ training-react/src/app/components/Board/index.js
@@ -1,47 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Square from '~components/Square';
 
 import style from './styles.scss';
-import calculateWinner from './utils.js';
 
 class Board extends Component {
-  state = {
-    squares: Array(9).fill(null),
-    xIsNext: true
-  };
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ squares, xIsNext: !this.state.xIsNext });
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = winner === 'Tie!' ? winner : `Winner: ${winner}`;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
-
     const cells = Array(9);
 
     for (let i = 0; i < cells.length; i += 1) {
-      cells[i] = <Square key={i} pos={i} value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+      cells[i] = (
+        <Square key={i} pos={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
+      );
     }
 
-    return (
-      <div className={style.boardAndStatus}>
-        <div className={style.status}>{status}</div>
-        <div className={style.board}>{cells}</div>
-      </div>
-    );
+    return <div className={style.board}>{cells}</div>;
   }
 }
+
+Board.propTypes = {
+  squares: PropTypes.Array,
+  onClick: PropTypes.func
+};
 
 export default Board;
--- training-react/src/app/components/Game/index.js
+++ training-react/src/app/components/Game/index.js
@@ -3,14 +3,45 @@ import React, { Component } from 'react';
 import Board from '~components/Board';
 
 import style from './styles.scss';
+import calculateWinner from './utils.js';
 
 class Game extends Component {
+  state = {
+    history: [{ squares: Array(9).fill(null) }],
+    xIsNext: true
+  };
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{ squares }]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = winner === 'Tie!' ? winner : `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
     return (
       <div className={style.game}>
-        <Board />
+        <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         <div className={style.gameInfo}>
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
