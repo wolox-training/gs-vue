065849ed899503625b6192e12c6a7b15453dc696
GiulianoScaglioni <giuliano.scaglioni@wolox.com.ar>
2019-01-09 14:05:54 -0300

    Added register layout

--- training-vue/wolox-books/src/App.vue
+++ training-vue/wolox-books/src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/register">Register</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background-color: rgb(244, 244, 244);
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
 }
 #nav {
   padding: 30px;
@@ -24,6 +29,38 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+    &:not(:last-child) {
+      margin-right: 10px;
+    }
   }
+}
+input {
+  border: 0;
+  border-radius: 10px;
+  padding: 10px;
+  font-size: 16px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+}
+label {
+  font-weight: 700;
+  font-size: 14px;
+}
+button {
+  border: solid 2px rgb(182, 207, 93);
+  background-color: rgb(182, 207, 93);
+  color: #fff;
+  border-radius: 10px;
+  padding: 15px;
+  line-height: 14px;
+  font-size: 14px;
+  width: 400px;
+  box-sizing: border-box;
+  font-weight: 600;
+  margin-bottom: 20px;
+}
+.btn-outline {
+  background-color: transparent;
+  color: rgb(182, 207, 93);
 }
 </style>
---
+++ training-vue/wolox-books/src/views/Register.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <form class="register-form">
+      <label>First name</label>
+      <input type="text">
+      <label>Last name</label>
+      <input type="text">
+      <label>Email</label>
+      <input type="email">
+      <label>Password</label>
+      <input type="password">
+      <button>Sign up</button>
+    </form>
+    <button class="btn-outline">Login</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.register-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 2px solid rgb(223, 223, 223);
+  margin-bottom: 20px;
+}
+input,
+label {
+  width: 400px;
+  text-align: left;
+}
+</style>
