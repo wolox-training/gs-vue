63e86293cfdfc2ad4d8f6bb5dd28ad09ee7e2e8f
GiulianoScaglioni <giuliano.scaglioni@wolox.com.ar>
2019-01-10 16:56:50 -0300

    Remove input and button components

--- training-vue/wolox-books/src/views/Register.vue
+++ training-vue/wolox-books/src/views/Register.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="container">
     <form class="register-form" @submit.prevent="onSubmit">
-      <WoloxInput name="first-name" label="First name" type="text" v-model="firstName"/>
-      <WoloxInput name="last-name" label="Last name" type="text" v-model="lastName"/>
-      <WoloxInput name="email" label="Email" type="email" v-model="email"/>
-      <WoloxInput name="password" label="Password" type="password" v-model="password"/>
-      <WoloxButton type="submit">Sign up</WoloxButton>
+      <label class="label">First name</label>
+      <input class="input-wolox" type="text" name="first-name" v-model="firstName" />
+      <label class="label">Last name</label>
+      <input class="input-wolox" type="text" name="last-name" v-model="lastName" />
+      <label class="label">Email</label>
+      <input class="input-wolox" type="email" name="email" v-model="email" />
+      <label class="label">Password</label>
+      <input class="input-wolox" type="password" name="password" v-model="password" />
+      <button class="btn-wolox" type="submit">Sign up</button>
     </form>
-    <WoloxButton type="button" variant="outline">Login</WoloxButton>
+    <button class="btn-wolox-outline" type="button">Login</button>
   </div>
 </template>
 
 <script>
-import WoloxInput from '@/components/WoloxInput.vue'
-import WoloxButton from '@/components/WoloxButton.vue'
-
 export default {
   name: 'register',
   components: {
-    WoloxInput,
-    WoloxButton
   },
   data () {
     return {
@@ -60,8 +59,7 @@ export default {
 .register-form {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   border-bottom: 2px solid rgb(223, 223, 223);
   margin-bottom: 20px;
   width: 100%;
