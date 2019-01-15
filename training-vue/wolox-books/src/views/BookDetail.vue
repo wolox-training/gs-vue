53ead58db32b37131a3051badd31105f935544fa
GiulianoScaglioni <giuliano.scaglioni@wolox.com.ar>
2019-01-15 14:37:14 -0300

    Added book detail view and route auth param

---
+++ training-vue/wolox-books/src/views/BookDetail.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .dashboard-container
+      wolox-navbar.m-bottom-2
+      .h1
+        | {{`Showing book ${id}`}}
+</template>
+
+<script>
+import WoloxNavbar from '@/components/WoloxNavbar'
+
+export default {
+  name: 'BookDetail',
+  components: {
+    WoloxNavbar
+  },
+  props: {
+    id: Number
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/variables/colors';
+
+.dashboard-container {
+  background-color: $wild-sand;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
