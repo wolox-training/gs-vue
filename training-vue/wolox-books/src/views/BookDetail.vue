ea6d92cdc5b9708d9842706b96328174e1e77b2c
GiulianoScaglioni <giuliano.scaglioni@wolox.com.ar>
2019-01-15 16:00:18 -0300

    Book detail layout

--- training-vue/wolox-books/src/views/BookDetail.vue
+++ training-vue/wolox-books/src/views/BookDetail.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
   .dashboard-container
     wolox-navbar.m-bottom-2
-    h1
-      | {{`Showing book ${id}`}}
-    div
-      | {{JSON.stringify(book)}}
+    .card(v-if="book")
+      img.image(:src="book.image_url")
+      .head
+        h1.title.bold.m-right-1
+          | {{book.title}}
+        span
+          | ({{book.genre}})
+      .more-info
+        span.m-bottom-2
+          span.bold.p-right-1
+            | Book author:
+          | {{book.author}}
+        span.m-bottom-2
+          span.bold.p-right-1
+            | Publisher:
+          | {{book.publisher}}
+        span
+          span.bold.p-right-1
+            | Year of publication:
+          | {{book.year}}
+    span(v-else)
+      | Loading book...
 </template>
 
 <script>
@@ -16,7 +34,7 @@ export default {
     WoloxNavbar
   },
   props: {
-    id: Number
+    id: String
   },
   mounted () {
     this.$store.dispatch('books/loadDetail', this.id)
@@ -41,5 +59,40 @@ export default {
   flex-direction: column;
   align-items: center;
   min-height: 100vh;
+}
+
+.card {
+  background-color: $white;
+  box-shadow: 2px 2px 0 $black-alpha-15;
+  display: grid;
+  grid-template-areas: 'image title'
+    'image details';
+  grid-template-rows: min-content 1fr;
+  grid-template-columns: 250px 750px;
+  gap: 20px;
+  padding: 20px;
+}
+
+.image {
+  grid-area: image;
+  width: 250px;
+  height: 250px;
+  object-fit:scale-down;
+  background-color: $wild-sand;
+}
+
+.head {
+  border-bottom: 2px solid $celery;
+  padding: 10px;
+}
+
+.title {
+  font-size: 24px;
+}
+
+.more-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 </style>
